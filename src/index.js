ee49acb632bd493a83b64c2a4b6bc952fd0c3bb9
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2025-01-02 23:28:18 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+
 const { 
   merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
   textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, 
   mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
-  wikimedia, tiktokDl, xdown 
+  wikimedia, tiktokDl, xdown, mediafire, googleImage, gitclone
 } = require('./api-qasim');
+const { igdl } = require('./insta');
+const { fbdl } = require('./fb');
 
 const Qasim = {
   merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
@@ -13,7 +16,7 @@ const Qasim = {
   textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, 
   mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
-  wikimedia, tiktokDl, xdown
+  wikimedia, tiktokDl, xdown, igdl, fbdl, mediafire, googleImage, gitclone
 };
 
 module.exports = Qasim;
