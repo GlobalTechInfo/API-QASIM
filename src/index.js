632083250a7f65fc6e72960157f1225f5f7f6e71
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2025-01-07 21:38:27 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
+// api-qasim.js
 const { 
-  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
-  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
-  apkmirror, weather, apksearch, happymod, nickff, nickml, 
-  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly, NiatFajar, NiatZuhur, NiatAshar, NiatMaghrib, NiatIsha,
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, BacaanSalaht, NiatSalaht, QisaNabi, AsmaulHusna, 
+  textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, DoaHarian, Wirid, Tahlil, AyatKursi,
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, Game, Technology, Programming, CyberSpace, Islamic, Mountain,
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
-  tiktokDl, xdown, mediafire, googleImage, gitclone
+  wikimedia, tiktokDl, xdown, mediafire, googleImage, gitclone, githubStalk, tiktokStalk, freefireStalk, igStalk, Remini, npmStalk, Pinterest2
 } = require('./api-qasim');
 const { igdl } = require('./insta');
 const { fbdl } = require('./fb');
 
 const Qasim = {
-  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
-  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
-  apkmirror, weather, apksearch, happymod, nickff, nickml, 
-  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
-  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
-  tiktokDl, xdown, igdl, fbdl, mediafire, googleImage, gitclone
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly, NiatFajar, NiatZuhur, NiatAshar, NiatMaghrib, NiatIsha,
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, BacaanSalaht, NiatSalaht, QisaNabi, AsmaulHusna, 
+  textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, DoaHarian, Wirid, Tahlil, AyatKursi,
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, igdl, fbdl,
+  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, Game, Technology, Programming, CyberSpace, Islamic, Mountain,
+  wikimedia, tiktokDl, xdown, mediafire, googleImage, gitclone, githubStalk, tiktokStalk, freefireStalk, igStalk, Remini, npmStalk, Pinterest2
 };
 
 module.exports = Qasim;
