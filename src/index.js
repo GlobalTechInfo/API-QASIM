3617dcac902e119a93cb3b9d89919bf105dc27d6
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2025-01-11 01:09:28 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ const {
 } = require('./api-qasim');
 const { igdl } = require('./insta');
 const { fbdl } = require('./fb');
+const { ytmp3, ytmp4 } = require('./youtube');
+const { ytsearch } = require('./ytsearch');
 
 const Qasim = {
   merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly, NiatFajar, NiatZuhur, NiatAshar, NiatMaghrib, NiatIsha,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, BacaanSalaht, NiatSalaht, QisaNabi, AsmaulHusna, 
   textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, DoaHarian, Wirid, Tahlil, AyatKursi,
-  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, igdl, fbdl,
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, igdl, fbdl, ytmp3, ytmp4, ytsearch,
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, Game, Technology, Programming, CyberSpace, Islamic, Mountain,
   wikimedia, tiktokDl, xdown, mediafire, googleImage, gitclone, githubStalk, tiktokStalk, freefireStalk, igStalk, Remini, npmStalk, Pinterest2
 };
